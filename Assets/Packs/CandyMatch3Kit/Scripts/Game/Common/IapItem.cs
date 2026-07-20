1727017b96637f031a82d0e30aeffdf62c564547
Nichita Levandovici <nichitalnc@gmail.com>
2026-07-20 17:55:45 +0300

    fix

--- Assets/Packs/CandyMatch3Kit/Scripts/Game/Common/IapItem.cs
+++ Assets/Packs/CandyMatch3Kit/Scripts/Game/Common/IapItem.cs
@@ -2,11 +2,11 @@
 // This code can only be used under the standard Unity Asset Store End User License Agreement,
 // a copy of which is available at http://unity3d.com/company/legal/as_terms.
 
-#if UNITY_EDITOR
 using System;
+#if UNITY_EDITOR
 using UnityEditor;
-using UnityEngine;
 #endif
+using UnityEngine;
 
 namespace GameVanilla.Game.Common
 {
